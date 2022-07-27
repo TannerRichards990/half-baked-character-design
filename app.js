32404c3cf352a92447a66b1932e5dfc527249418
Tanner Richards <Tanner.richards990@gmail.com>
2022-07-27 16:01:33 -0700

    added textcontent to the function displaystats so that the user knows how many times they changed outfits.

--- app.js
+++ app.js
@@ -73,6 +73,9 @@ catchphraseButton.addEventListener('click', () => {
 
 function displayStats() {
     // text content of the reportEl to tell the user how many times they've changed each piece of the state
+    reportEl.textContent = `You have changed the head ${headCount} times, the body ${middleCount} times, 
+    and the pants ${bottomCount} times. And nobody can forget your character's classic catchphrases:`;
+
 }
 
 function displayCatchphrases() {
